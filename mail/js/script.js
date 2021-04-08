f603b8b627e32f2939bdec9f038278e94bcaf9f5
marcomanelli <tronmsi@gmail.com>
2021-04-08 16:54:09 +0200

    fix

--- mail/js/script.js
+++ mail/js/script.js
@@ -2,11 +2,17 @@ var mail = prompt('Inserisci la tua mail:')
 
 var list = ['gigio@gmail.com', 'luigi@gmail.com', 'marcello@gmail.com', 'luisa@gmail.com', 'elisa@gmail.com']
 
+var confirmed = false
+
 for (var i=0; i<list.length; i++){
   if (mail == list[i]){
-    alert('Accesso Confermato')
-  }else{
-    alert('Accesso Negato')
+    confirmed = true
     break
   }
+}
+
+if (confirmed == true){
+  alert('Accesso Consentito')
+}else{
+  alert('Accesso Negato')
 }
